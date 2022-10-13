2d558d55ee0086d0daa5449ecc98e1ce5572f70e
DOUGLAS MESSIAS DA SILVA <douglas.msilva32@senacsp.edu.br>
2022-10-13 17:10:52 -0300

    Enviar para o bando de dados

---
+++ app/Http/Controllers/CaminhaoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\support\Facades\Redirect;
+use Illuminate\Http\Request;
+use App\Models\Caminhao;
+
+class CaminhaoController extends Controller
+{
+    public function FormularioCadastro(){
+        return view('cadastrarCaminhao');
+    }
+
+    public function Editar(){
+        return view('editarCaminhao');
+    }
+
+    public function SalvarBanco(Request $request){
+        $dadosCaminhao = $request->validate([
+            'modelos' => 'string|required',
+            'marca' => 'string|required',
+            'ano' => 'string|required',
+            'cor' => 'string|required',
+            'valor' => 'string|nullable'
+        ]);
+        
+        Caminhao::create($dadosCaminhao);
+
+        return Redirect::route('home');
+    }
+
+}
---
+++ app/Models/Caminhao.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Caminhao extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'modelos', 'marca', 'ano', 'cor', 'valor'
+    ];
+}
